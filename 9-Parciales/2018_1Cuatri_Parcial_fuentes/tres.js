af04dc016d8c418dd945bc29656202943c1214ef
hngonzalez <40776472+hngonzalez@users.noreply.github.com>
2018-07-04 16:10:57 -0300

    Parcial
    Parcial en casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
 
+    var precioInicial;
+    var porcenDescuento;
+    var precioFinal;
+
+    precioIinicial = prompt("Ingrese el precio: ");
+    porcenDescuento = prompt("Cargue el porcentaje de descuento: ");
+    
+    precioFinal = precioInicial * porcenDescuento / 100;
+    
+    document.getElementById('elPrecioFinal').value = precioFinal;
+    
+    //El resultado me aparece como NaN
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,14 @@
 function mostrar()
 {
 
+    var largo;
+    var ancho;
+    var perimetro;
+
+    largo = prompt("Ingrese el largo ");;
+    ancho = prompt("Ahora ingrese el ancho ");
+    perimetro = (largo*2) + (ancho*2);
+
+    alert("El perímetro del rectángulo es: "+perimetro);
+
 }
