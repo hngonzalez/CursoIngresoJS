0ff4ef086fb4207dd6807f0bc20b23f6d6a8b439
hngonzalez <40776472+hngonzalez@users.noreply.github.com>
2018-07-17 20:05:02 -0300

    a
    a

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,48 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño) {
+    case "Enero":
+        alert("que comiences bien el año!!!.");
+        break;
+    case "Febrero":
+    
+        break;
+    case "Marzo":
+        alert("a clases!!!.");
+        break;
+    case "Abril":
+    
+        break;
+    case "Mayo":
+    
+        break;
+    case "Junio":
+    
+        break;
+    case "Julio":
+        alert("se vienen las vacaciones!!!.");
+        break;
+    case "Agosto":
+    
+        break;
+    case "Septiembre":
+    
+        break;
+    case "Octubre":
+    
+        break;
+    case "Noviembre":
+    
+        break;
+    case "Diciembre":
+        alert("Felices fiesta!!!.");
+        break;
+
+    default:
+
+        break;
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,11 +2,10 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
+laHora = parseInt(laHora);
 
 //alert (laHora);
 	
-	
-
 
 
 }//FIN DE LA FUNCIÓN
