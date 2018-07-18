4bc635a1d8c9bb2dd11232cd28825ce218a57a71
hngonzalez <40776472+hngonzalez@users.noreply.github.com>
2018-07-18 10:49:48 -0300

    Update jsPiedarPapelTijera1.js

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,6 +5,7 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var eleccionHumano;
 
 function comenzar()
 {
@@ -14,22 +15,34 @@ function comenzar()
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	if (eleccionMaquina == 1) {
-        
+	if (eleccionHumano == 1 && eleccionMaquina == 3) {
+        alert("El usuario resultó ganador")
+    } else if (eleccionHumano == 1 && eleccionMaquina == 2){
+    	
+    } else if (eleccionHumano == 1 && eleccionMaquina == 1){
+    	
     }
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-    if (eleccionMaquina == 2) {
-        
+    if (eleccionMaquina == 2 && eleccionHumano == 1) {
+        alert("El usuario resultó ganador")
+    } else if (eleccionHumano == 2 && eleccionMaquina == 3){
+    	
+    } else if (eleccionHumano == 2 && eleccionMaquina == 2){
+    	
     }
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	if (eleccionMaquina == 3) {
-        
+	if (eleccionMaquina == 3 && eleccionHumano == 2) {
+        alert("El usuario resultó ganador")
+    } else if (eleccionHumano == 3 && eleccionMaquina == 1){
+    	
+    } else if (eleccionHumano == 3 && eleccionMaquina == 3){
+    	
     }
 
 }//FIN DE LA FUNCIÓN
