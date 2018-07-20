4b1cc5a16a4a9a29d76e3065ab6f9d943e3d344f
hngonzalez <40776472+hngonzalez@users.noreply.github.com>
2018-07-20 09:06:11 -0300

    Update jsInstruccionSwitch(5).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -4,8 +4,28 @@ function mostrar()
 var laHora = document.getElementById('hora').value;
 laHora = parseInt(laHora);
 
+switch (laHora){
+	case 7:
+		alert("Es de mañana.");
+		break;
+	case 8:
+		alert("Es de mañana.");
+		break;
+	case 9:
+		alert("Es de mañana.");
+		break;
+	case 10:
+		alert("Es de mañana.");
+		break;
+	case 11:
+		alert("Es de mañana.");
+		break;
+	default:
+		break;
+		
+}
 //alert (laHora);
-	
+
 
 
 }//FIN DE LA FUNCIÓN
