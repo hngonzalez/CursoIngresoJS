86b8dfb40f9305671836d41c9565f02cb65157f1
hngonzalez <40776472+hngonzalez@users.noreply.github.com>
2018-07-23 10:58:15 -0300

    Update jsAgilidadAritmetica1.js

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -6,15 +6,31 @@ y una de las cuatro operaciones básicas (suma, resta,
  el resultado de la operación y presionar el botón Aceptar.
 se debe informar si el resultado es el correcto o no.
 */
+var numeroRandomA;
+var numeroRandomB;
+var operacionMas;
+var operacionResta;
+var operacionMultiplicar;
+var operacionDividir;
 var respuesta;
+
 function comenzar()
 {
 
-	
+numeroRandomA = Math.floor(Math.random()*10)+1;
+numeroRandomA = parseInt(numeroRandomA);
+numeroRandomB = Math.floor(Math.random()*10)+1;
+numeroRandomB = parseInt(numeroRandomB);
+
+}
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+
+operacionMas = eval("numeroRandomA + numeroRandomB");
+operacionResta = eval("numeroRandomA - numeroRandomB");
+operacionMultiplicar = eval("numeroRandomA * numeroRandomB");
+operacionDividir = eval("numeroRandomA + numeroRandomB");
 
 }//FIN DE LA FUNCIÓN
