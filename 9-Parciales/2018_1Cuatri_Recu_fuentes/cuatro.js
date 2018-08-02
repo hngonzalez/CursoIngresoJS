16a0fc83083ff4740c5c8b43e5311651c6fbc3d8
hngonzalez <40776472+hngonzalez@users.noreply.github.com>
2018-08-02 08:50:30 -0300

    Update cuatro.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+  var numeroUno;
+  var numeroDos;
+  var resultado;
+  
+  if (numeroUno == numeroDos){
+    alert("Números concatenados: "+numeroUno+numeroDos);
+  } else if (numeroUno > numeroDos){
+    resultado = numeroUno / numeroDos;
+  } else {
+    resultado = numeroUno + numeroDos;
+    if (resultado < 50) {
+        
+    }
+  } 
 
 }
